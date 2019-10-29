d320dbf013f47a056cb9e7ca6abf79bb0ebcc7c3
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2019-10-29 15:02:02 +1300

    Add files via upload

---
+++ JavaSort/src/javasort/CatalogueItem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasort;
+
+/**
+ *
+ * @author Peter Seatter
+ */
+class CatalogueItem {
+      
+    private Integer id;
+    private String itemName;
+    private String category;
+    
+    // getter methods
+    public int getItemId(){
+    return id;
+    }
+    
+    public String getItemName(){
+    return itemName;
+    }
+    
+    public String getCategory(){
+    return category;
+    }
+    
+    // constructor
+    public CatalogueItem(Integer newId,String newName, String newCategory){
+
+        id = newId;
+        itemName = newName;
+        category = newCategory;
+    }
+    
+}
---
+++ JavaSort/src/javasort/JavaSort.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasort;
+
+import java.util.Arrays;
+import static java.util.Arrays.sort;
+import java.util.Comparator;
+
+/**
+ *
+ * @author Peter Seatter
+ */
+public class JavaSort {
+//    implements Comparator<CatalogueItem>
+    
+    /* Prints the array */
+    void printArray(CatalogueItem arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(
+                    "id: " + arr[i].getItemId() + " "
+                    + "name: " + arr[i].getItemName() + " "
+                    + "category: " + arr[i].getCategory() + "\n");
+        System.out.println();
+        System.out.println();
+    }
+    
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        JavaSort ob = new JavaSort();
+        
+        CatalogueItem arr[] = {
+            new CatalogueItem( 3, "Life of Pi","Books"),
+            new CatalogueItem( 7, "Deelongie 4 way toaster","Home and Kitchen"),
+            new CatalogueItem( 2, "Glorbarl knife set","Home and Kitchen"),
+            new CatalogueItem( 4, "Diesorn vacuum cleaner","Appliances"),
+            new CatalogueItem( 5, "Jennie Olivier sauce pan","Home and Kitchen"),
+            new CatalogueItem( 6, "This book will save your life","Books"),
+            new CatalogueItem( 9, "Kemwould hand mixer","Appliances"),
+            new CatalogueItem( 1, "Java for Dummies","Books"),
+        };
+        System.out.println("The Unsorted array is");
+        ob.printArray(arr);
+        
+         //apply sort
+//        ob.doJavaSort(arr);
+        
+        Arrays.sort(arr);
+        System.out.println("The array sorted by category using Java built in sort is");
+        ob.printArray(arr);
+    }
+}
